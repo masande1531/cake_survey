6135ad54f0104263f8eecab4723d48c7542af1f5
masande1531 <masande@globalimsa.com>
2014-02-20 11:28:24 +0200

    Create Survey.php
    Survey Model

---
+++ app/Model/Survey.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Survey
+ *
+ * @author Masande
+ */
+
+class Survey extends AppModel{
+    
+}
+?>
