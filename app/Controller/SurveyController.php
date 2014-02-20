37c9c3fdd7ca87a9164934ae72b625e0f8f930de
masande1531 <masande@globalimsa.com>
2014-02-20 11:27:20 +0200

    Create SurveyController.php
    SurveryController

---
+++ app/Controller/SurveyController.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class SurveyController extends AppController{
+    public $helper = array('Html', 'Form');
+    
+    public function index(){
+        $this->set('surveys', $this->Survey->find('all'));
+    }
+    
+    
+    public function add(){
+            if ($this->request->is('post')) {
+            $this->Survey->create();
+            if ($this->Survey->save($this->request->data)) {
+                $this->Session->setFlash(__('Your post has been saved'));
+                return $this->redirect(array('action' => 'index'));
+            }
+            $this->Session->setFlash(__('Unable to add your post.'));
+        }
+    }
+}
+?>
+
